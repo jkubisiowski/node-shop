4083e21e911d9e351ada1009784ba2d637d9fce5
Jakub Kubisiowski <jakub.kubisiowski@fortedigital.no>
2020-03-05 19:41:06 +0100

    Add pug rendering engine

--- routes/shop.js
+++ routes/shop.js
@@ -2,9 +2,12 @@ const express = require('express');
 const router = express.Router();
 const path = require('path');
 const rootDir = require('../util/path');
+const adminData = require('./admin');
 
 router.get('/', (req, res, next) => {
-    res.sendFile(path.join(rootDir, 'views', 'shop.html'));
+    console.log(adminData.products);
+    const products = adminData.products;
+    res.render('shop', {prods: products, docTitle: 'Shoplo'})
 });
 
 module.exports = router;
